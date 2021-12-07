b0cc3dd7487eadcb8e3cb1250725b0e2640db472
Lauri <1960498+msha@users.noreply.github.com>
2021-12-07 22:31:27 +0200

    add functionality to add images, add basic version of dynamic onfly text entering

--- src/codeops.py
+++ src/codeops.py
@@ -8,3 +8,11 @@ class Code:
 
     def save_code(self,new_code):
         self.code = new_code
+
+    def insert_code(self,new_code):
+        if new_code == '\x08':
+            self.code = self.code[:-1]
+        elif new_code == '\r':
+            self.code += '<br>'
+        else:
+            self.code += new_code
--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,5 @@
-from tkinter import Menu,Frame,PhotoImage,Button,Toplevel,Text,Tk,RIGHT,BOTTOM,YES
+from tkinter import Menu,Frame,PhotoImage,Button,Toplevel,Text,Tk,filedialog,RIGHT,BOTTOM,YES
+from tkinter.constants import TOP
 from tkhtmlview import HTMLLabel
 from codeops import Code
 from fileops import Fileops
@@ -13,7 +14,7 @@ class Gui:
 
 
     def menu(self):
-
+        '''Top menu in GUI'''
 
         menubar = Menu(self.root)
         filemenu = Menu(menubar, tearoff=0)
@@ -31,6 +32,7 @@ class Gui:
         Fileops.savefile(self,"testi.html",self.code.read_code())
 
     def tools(self):
+        '''Tool area in GUI'''
         button_frame = Frame(self.root)
         button_frame.pack()
         bg_icon = PhotoImage(file='./img/background.png')
@@ -40,13 +42,25 @@ class Gui:
             text ="Background",
             command = '')
         self.button_background.pack(side = RIGHT)
-        add_text = Button(
+        view_code = Button(
             button_frame,
             text ="Code",
-            command = self.create_window)
-        add_text.pack(side = RIGHT,expand=YES)
+            command = self.create_window_code)
+        view_code.pack(side = RIGHT,expand=YES)
+        text_tools = Button(
+            button_frame,
+            text ="Text",
+            command = self.create_window_code)
+        text_tools.pack(side = RIGHT,expand=YES)
+        add_image = Button(
+            button_frame,
+            text ="Add Image",
+            command = self.create_window_image)
+        add_image.pack(side = RIGHT,expand=YES)
 
-    def create_window(self):
+
+    def create_window_code(self):
+        '''Window for viewing code'''
         window = Toplevel(self.root)
         window.geometry("450x250")
         tekstin_syotto = Text(
@@ -67,14 +81,50 @@ class Gui:
             command = close)
         save.pack(side = BOTTOM)
 
+    def create_window_image(self):
+        ''''Window for adding images'''
+        window = Toplevel(self.root)
+        window.geometry("450x250")
+        filepath = Text(
+            window,
+            height=1,
+            width=40
+        )
+        def open_file():
+            file_path = filedialog.askopenfilename(parent=window,
+                filetypes=[("Images","*.jpg *.png *.gif *.webp *.jpeg")],title='Choose a file')
+            filepath.insert('end',file_path)
+
+        def close():
+            self.code.insert_code('<img src='+filepath.get(1.0,'end')+'></img>')
+            window.destroy()
+
+        browse = Button(
+            window,
+            text ="Browse images",
+            command = open_file)
+        save = Button(
+            window,
+            text ="Save",
+            command = close)
+        filepath.pack(side=TOP)
+        browse.pack(side=TOP,pady=20)
+        save.pack(side = BOTTOM)
 
     def html_area(self):
+        '''Render of the work in progress HTML'''
         self.htmlview.set_html(self.code.read_code(),True)
-        self.htmlview.after(1000,self.html_area)
+        self.htmlview.after(10,self.html_area)
+
+    def on_key_press(self,event):
+        '''Listing to keyevent and converting them to inputs'''
+        print((event.char, ))
+        self.code.insert_code(event.char)
 
     def start(self):
         self.menu()
         self.tools()
         self.htmlview.pack(fill="both", expand=True)
         self.html_area()
+        self.root.bind('<KeyPress>', self.on_key_press)
         self.root.mainloop()
